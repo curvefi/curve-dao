27ad95da4de80be955ac3a330debbf6e0b84587a
AA <angelangelov97@gmail.com>
2020-03-21 15:15:52 +0200

    add gzip compression on build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 process.env.VUE_APP_VERSION = require('./package.json').version
 
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = {
+	chainWebpack(config) {
+		config.plugin('CompressionPlugin').use(CompressionPlugin)
+	},
     // options...
     devServer: {
         disableHostCheck: true

3070758b7784688bd0662d5dd687a0806642cbc5
AA <angelangelov97@gmail.com>
2020-03-21 16:00:52 +0200

    better without compression :D

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,6 @@
 process.env.VUE_APP_VERSION = require('./package.json').version
 
-const CompressionPlugin = require('compression-webpack-plugin');
-
 module.exports = {
-	configureWebpack: {
-		plugins: [
-			new CompressionPlugin({ deleteOriginalAssets: true })
-		]
-	},
     // options...
     devServer: {
         disableHostCheck: true

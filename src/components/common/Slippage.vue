3ac03cdfb060fc4b72b5c3407f18ee76b9af143a
AA <angelangelov97@gmail.com>
2020-04-22 22:11:39 +0300

    slippage + pricing

--- src/components/common/Slippage.vue
+++ src/components/common/Slippage.vue
@@ -1,20 +1,20 @@
 <template>
 	<div>
 	    <p class='simple-error' id='highslippage-warning' v-show='showSlippage && slippage < -0.005'>
-	    	<span class='text'>Warning! High slippage</span>: 
+	    	<span class='text'>Warning! High slippage(plus pricing)</span>: 
 	    	<span class='percent'>{{(-slippage*100).toFixed(3)}}</span>%
 	    </p>
 	    <p class='info-message' id='slippage-warning' v-show='showSlippage && slippage <= 0 && slippage >= -0.005'>
-	    	<span class='text'>Slippage</span>: 
+	    	<span class='text'>Slippage(plus pricing)</span>: 
 	    	<span class='percent'>{{(-slippage*100).toFixed(3)}}</span>%
 	    </p>
 	    <p class='success' id='bonus-window' v-show='showSlippage && slippage > 0'>
-	    	Bonus: <span>{{(slippage*100).toFixed(3)}}</span>%
+	    	Bonus(plus pricing): <span>{{(slippage*100).toFixed(3)}}</span>%
 	    </p>
       <p class='simple-error' id='nobalance-warning' v-show='show_nobalance'>
       	Warning! Not enough balance for {{noBalanceText}} token in the contract
       </p>
-      <p class='info-message gentle-message' id='susd-warning' v-show="currentPool == 'susdv2' && showSlippage && slippage < -0.001">
+      <p class='info-message gentle-message' id='susd-warning' v-show="currentPool == 'susdv2' && showSlippage && slippage < -0.01">
         Please add coins in a balanced proportion
       </p>
 	</div>

4ef03f6df49ae17a46c23acb0c1da2542ae160e5
AA <angelangelov97@gmail.com>
2020-04-30 22:02:13 +0300

    fix

--- src/components/ProfitMixin.js
+++ src/components/ProfitMixin.js
@@ -378,6 +378,7 @@ export default {
 		        }
 		        else {
 		            let transfer = receipt.logs.filter(log=>log.topics[0] == this.TRANSFER_TOPIC && log.topics[1] == '0x000000000000000000000000' + default_account)
+		            if(transfer[0].topics[2] == "0x000000000000000000000000dcb6a51ea3ca5d3fd898fd6564757c7aaec3ca92") continue;
 		            let tokens = +transfer[0].data
 		            let poolInfoPoint = this.findClosest(timestamp)
 		            let usd = this.getAvailableTransfer(tokens, poolInfoPoint.balances, poolInfoPoint.supply)

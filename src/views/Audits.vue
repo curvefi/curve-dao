85e3839b01666bb087dc51120138ecb68809e381
AA <angelangelov97@gmail.com>
2020-05-01 06:45:47 +0300

    change audits links

--- src/views/Audits.vue
+++ src/views/Audits.vue
@@ -4,13 +4,13 @@
 	            <legend>Audits</legend>
 	            <div id='auditcontainer'>
 		            <div>
-		                <a href='https://www.curve.fi/audits/00-ToB.pdf'>
+		                <a href='https://www.curve.fi/curve_audits/00-ToB.pdf'>
 		                	<img class='icon' src="../assets/trail-of-bits.jpeg?rev=98185d4fe81a0ff9206e8b34c66e2be5">
 		                	<span class='text'>Audit 31.01.2020</span>
 		                </a>
 		            </div>
 		            <div>
-		                <a href='https://www.curve.fi/audits/01-ToB.pdf'>
+		                <a href='https://www.curve.fi/curve_audits/01-ToB.pdf'>
 		                	<img class='icon' src='../assets/trail-of-bits.jpeg?rev=8160e2e87988f42e44fd33495dead52c' />
 		                	<span class='text'>Audit 06.03.2020</span>
 		                </a>
@@ -25,17 +25,17 @@
 		metaInfo: {
 	      title: 'Curve.fi :: Audits',
 	      meta: [
-	        {'property': 'og:title', 'content': 'curve.fi/audits'},
-	        {'property': 'og:url', 'content': 'https://curve.fi/audits'},
+	        {'property': 'og:title', 'content': 'curve.fi/curve_audits'},
+	        {'property': 'og:url', 'content': 'https://curve.fi/curve_audits'},
 	        {'property': 'og:type', 'content': 'website'},
 	        {'property': 'og:description', 'content': 'Curve is an exchange liquidity pool on Ethereum designed for extremely efficient stablecoin trading'},
 	        {'property': 'og:image', 'content': '/curve.png'},
 	        {'name': 'twitter:card', 'content': 'summary_large_image'},
-	        {'name': 'twitter:title', 'content': 'curve.fi/audits'},
+	        {'name': 'twitter:title', 'content': 'curve.fi/curve_audits'},
 	        {'name': 'twitter:site', 'content': '@CurveFinance'},
 	        {'name': 'twitter:creator', 'content': '@CurveFinance'},
 	        {'name': 'twitter:description', 'content': 'Curve is an exchange liquidity pool on Ethereum designed for extremely efficient stablecoin trading'},
-	        {'name': 'twitter:url', 'content': 'https://curve.fi/audits'},
+	        {'name': 'twitter:url', 'content': 'https://curve.fi/curve_audits'},
 	        {'name': 'twitter:image', 'content': '/curve.png'},
 	      ]
 	    },

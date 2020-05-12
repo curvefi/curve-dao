8a65647d574721880e502bac2b561e5923ea2262
AA <angelangelov97@gmail.com>
2020-05-12 03:18:13 +0300

    add PAX to CurvePay

--- src/components/CurvePay.vue
+++ src/components/CurvePay.vue
@@ -40,7 +40,7 @@
 	export default {
 		data: () => ({
 			token: 'cCrv',
-			tokenNames: ['cCrv', 'tCrv', 'yCrv', 'bCrv', 'sCrv'],
+			tokenNames: ['cCrv', 'tCrv', 'yCrv', 'bCrv', 'sCrv', 'pCrv'],
 			tokens: [],
 			contracts: [],
 			swaps: [],

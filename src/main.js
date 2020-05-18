a2864e29eb61a89ae3c20d7f926e5a0c41026d14
AA <angelangelov97@gmail.com>
2020-05-18 03:29:57 +0300

    add LogRocket

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import VueMeta from 'vue-meta'
 import * as Sentry from '@sentry/browser';
 import { CaptureConsole, Vue as VueIntegration } from '@sentry/integrations';
 
+import LogRocket from 'logrocket';
+
+
 Vue.use(VueMeta, {
   // optional pluginOptions
   refreshOnceOnNavigation: true
@@ -20,15 +23,26 @@ import '../public/tvision.css'
 Vue.config.productionTip = false
 
 //adding Sentry as soon as possible
-process.env.NODE_ENV == 'production' && Sentry.init({ 
-  dsn: 'https://5494f535e0244513a301f2912f5d899f@sentry.io/4169463',
-  integrations: [
-    new CaptureConsole({
-      levels: ['warn', 'error', 'debug', 'assert']
-    }),
-    new VueIntegration({Vue, attachProps: true})
-  ],
-});
+if(process.env.NODE_ENV == 'production') {
+  
+  Sentry.init({ 
+    dsn: 'https://5494f535e0244513a301f2912f5d899f@sentry.io/4169463',
+    integrations: [
+      new CaptureConsole({
+        levels: ['warn', 'error', 'debug', 'assert']
+      }),
+      new VueIntegration({Vue, attachProps: true})
+    ],
+  });
+
+  LogRocket.init('wsi23p/curve');
+  LogRocket.getSessionURL(sessionURL => {
+    Sentry.configureScope(scope => {
+      scope.setExtra("sessionURL", sessionURL);
+    });
+  });
+
+}
 
 
 new Vue({

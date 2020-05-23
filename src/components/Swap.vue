d002cc0d09e222077df385e5790c8ce9da99bc5b
AA <angelangelov97@gmail.com>
2020-05-23 05:00:46 +0300

    depositing zero warning after loda

--- src/components/Swap.vue
+++ src/components/Swap.vue
@@ -16,9 +16,10 @@
                                     ≈ {{actualFromValue}} {{Object.keys(currencies)[this.from_currency] | capitalize}}
                                 </p>
                             </li>
-                            <li v-for='(currency, i) in Object.keys(currencies)'>
+                            <li class='coins' v-for='(currency, i) in Object.keys(currencies)'>
                                 <input type="radio" :id="'from_cur_'+i" name="from_cur" :value='i' v-model='from_currency'>
                                 <label :for="'from_cur_'+i">
+                                    <img :class="{'icon token-icon': true, [currency+'-icon']: true}" :src='getTokenIcon(currency)'>
                                     <span v-show="!swapwrapped && !['tbtc', 'ren'].includes(currentPool)">{{currency | capitalize}}</span>
                                     <span v-show="swapwrapped || ['tbtc', 'ren'].includes(currentPool)">{{currencies[currency]}}</span>
                                 </label>
@@ -45,9 +46,10 @@
                                     ≈ {{actualToValue}} {{Object.keys(currencies)[this.to_currency] | capitalize}}
                                 </p>
                             </li>
-                            <li v-for='(currency, i) in Object.keys(currencies)'>
+                            <li class='coins' v-for='(currency, i) in Object.keys(currencies)'>
                                 <input type="radio" :id="'to_cur_'+i" name="to_cur" :value='i' v-model='to_currency'>
                                 <label :for="'to_cur_'+i">
+                                    <img :class="{'icon token-icon': true, [currency+'-icon']: true}" :src='getTokenIcon(currency)'>
                                     <span v-show="!swapwrapped && !['tbtc', 'ren'].includes(currentPool)">{{currency | capitalize}}</span>
                                     <span v-show="swapwrapped || ['tbtc', 'ren'].includes(currentPool)">{{currencies[currency]}}</span>
                                 </label>
@@ -209,6 +211,21 @@
                 }
                 this.disabled = false;
                 this.from_cur_handler()
+            },
+            getTokenIcon(token) {
+                if(token == 'dai') return require('../assets/tokens/dai4.png')
+                if(token == 'usdt') return require('../assets/tokens/usdt.svg')
+                if(token == 'susd') return require('../assets/tokens/susd1.svg')
+                //if(token == 'busd') return require('../assets/tokens/busd.svg')
+                if(token == 'busd') return require('../assets/tokens/busd1.png')
+                let asset
+                try {
+                    asset = require('../assets/tokens/' + token + '.png')
+                }
+                catch(err) {
+                    asset = require('../assets/tokens/' + token + '.svg')
+                }
+                return asset;
             },
             toFixed(num) {
                 if(!BigNumber.isBigNumber(num)) num = +num
@@ -406,5 +423,27 @@
     }
     #no-balance {
         text-align: center;
+    }
+    .coins {
+        margin-top: 0.3em;
+    }
+    .token-icon {
+        width: 1.4em;
+        height: 1.4em;
+        vertical-align: middle;
+        margin-right: 0.3em;
+        margin-left: 0.3em;
+        border: 1px solid #414141;
+        border-radius: 50%;
+    }
+    .dai-icon {
+    }
+    .susd-icon {
+        border-color: transparent;
+        width: 1.5em;
+        height: 1.5em;
+    }
+    .busd-icon {
+        border-color: transparent;
     }
 </style>

5a458eef1af2e00446fe307c5382478c8b84f0a4
AA <angelangelov97@gmail.com>
2020-05-28 08:12:06 +0300

    increase btc price retry delay

--- src/components/common/priceStore.js
+++ src/components/common/priceStore.js
@@ -5,7 +5,7 @@ export let state = {
 }
 
 export async function getBTCPrice() {
-	let req = await helpers.retry(fetch(`https://api.coinpaprika.com/v1/tickers/btc-bitcoin`));
+	let req = await helpers.retry(fetch(`https://api.coinpaprika.com/v1/tickers/btc-bitcoin`), 300);
     let res = await req.json();
     state.btcPrice = res.quotes.USD.price;
     return res.quotes.USD.price

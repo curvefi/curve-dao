be136d385958226b4ce69d7f1fb448c67a51a30e
AA <angelangelov97@gmail.com>
2020-05-28 08:11:06 +0300

    add retry for btc price

--- src/components/common/priceStore.js
+++ src/components/common/priceStore.js
@@ -1,9 +1,11 @@
+import * as helpers from '../../utils/helpers'
+
 export let state = {
 	btcPrice: null,
 }
 
 export async function getBTCPrice() {
-	let req = await fetch(`https://api.coinpaprika.com/v1/tickers/btc-bitcoin`);
+	let req = await helpers.retry(fetch(`https://api.coinpaprika.com/v1/tickers/btc-bitcoin`));
     let res = await req.json();
     state.btcPrice = res.quotes.USD.price;
     return res.quotes.USD.price
